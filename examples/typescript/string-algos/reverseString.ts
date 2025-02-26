0d3794884a3a104ecebc2a716d89327769ee04b4
Trevor Waters <142362719+Twaters623@users.noreply.github.com>
2025-02-26 21:29:10 +0000

    end 2/26

--- examples/typescript/string-algos/reverseString.ts
+++ examples/typescript/string-algos/reverseString.ts
@@ -1,13 +1,19 @@
 function reverseString(str: string): string {
   // using built-in string manipulation functions
-  // return str.split("").reverse().join("");
+  //   return str.split("").reverse().join("");
+
+  if (str === "") {
+    return "";
+  } else {
+    return reverseString(str.substr(1)) + str.charAt(0);
+  }
 
   // string concatenation using for loop
-  let reversedString: string = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversedString += str[i];
-  }
-  return reversedString;
+  //   let reversedString: string = "";
+  //   for (let i = str.length - 1; i >= 0; i--) {
+  //     reversedString += str[i];
+  //   }
+  //   return reversedString;
 }
 
 console.log(reverseString("Hello, World!"));
